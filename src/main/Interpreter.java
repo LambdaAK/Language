33ed8887a2c30160198c533bd7559d982159617d
LambdaAK <alex.kozik@yahoo.com>
2023-01-07 17:00:35 -0500

    File reading works. Removes trailing white space.

--- src/main/Interpreter.java
+++ src/main/Interpreter.java
@@ -4,25 +4,85 @@ import ast.Factor;
 import parse.Lexer;
 import parse.Parser;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Interpreter {
 
 
     public static void main(String[] args) {
 
 
-        String testInput = "1 - 2 + 3";
+
+        String input = "";
+
+        try {
+            input = read(args[0]);
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException");
+            System.exit(1);
+        }
 
 
-        Lexer lexer = new Lexer(testInput);
 
+
+
+
+        // lex
+
+
+        Lexer lexer = new Lexer(input);
 
         Parser parser = new Parser(lexer.tokens);
 
+        Factor root = parser.parseFactor();
 
-        Factor factor = parser.parseFactor();
+        System.out.println(root);
 
 
-        System.out.println(factor);
+
+
+    }
+
+
+    private static String read(String dir) throws IOException {
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(dir));
+        }
+
+        catch (FileNotFoundException e) {
+            System.err.println("File " + dir + " not found");
+            System.exit(1);
+        }
+
+
+
+        // now we can read the file
+        char[] buffer = new char[10];
+        StringBuilder builder = new StringBuilder();
+
+
+        while (reader.read(buffer) != -1) {
+            builder.append(new String(buffer));
+            buffer = new char[10];
+        }
+        reader.close();
+
+        String input = builder.toString();
+
+
+        // remove the trailing white space
+        while (input.charAt(input.length() - 1) == 0) {
+            input = input.substring(0, input.length() - 1); // remove the last character
+        }
+
+        return input;
 
 
 
